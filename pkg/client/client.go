353b296a802e629d9963bea8e0276e0aa7795d4e
Micah Kemp <micahk@splunk.com>
2022-01-18 11:17:48 -0600

    Added Client.Do

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -14,6 +14,91 @@
 
 package client
 
+import (
+	"crypto/tls"
+	"fmt"
+	"net/http"
+	"net/http/cookiejar"
+	"net/url"
+	"sync"
+
+	"github.com/splunk/go-sdk/pkg/internal/paths"
+	"golang.org/x/net/publicsuffix"
+)
+
 // Client defines connectivity and authentication to a Splunk REST API.
 type Client struct {
+	// URL is the URL to the Splunk REST API. It should include the scheme and port number.
+	//
+	// Example:
+	//   https://localhost:8089
+	URL string
+
+	// Authenticator defines which authentication method and credentials to use.
+	//
+	// Example:
+	//   authenticators.Password{Username: "admin", Password: "changeme"}
+	Authenticator
+
+	// Set TLSInsecureSkipVerify to true to skip TLS verification.
+	TLSInsecureSkipVerify bool
+
+	httpClient *http.Client
+	mu         sync.Mutex
+}
+
+// urlForPath returns a url.URL for path, relative to Client's URL.
+func (c *Client) urlForPath(path ...string) (*url.URL, error) {
+	if c.URL == "" {
+		return nil, fmt.Errorf("Client has empty URL")
+	}
+
+	combinedPath := paths.Join(path...)
+
+	u := paths.Join(c.URL, combinedPath)
+
+	return url.Parse(u)
+}
+
+// ServiceURL returns a url.URL for a Service, relative to the Client's URL.
+func (c *Client) ServiceURL(s Service) (*url.URL, error) {
+	servicePath, err := servicePath(s)
+	if err != nil {
+		return nil, err
+	}
+
+	return c.urlForPath(servicePath)
+}
+
+// httpClientPrep prepares the Client's http.Client.
+func (c *Client) httpClientPrep() error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if c.httpClient == nil {
+		jar, err := cookiejar.New(&cookiejar.Options{PublicSuffixList: publicsuffix.List})
+		if err != nil {
+			return fmt.Errorf("unable to create new cookiejar: %s", err)
+		}
+
+		c.httpClient = &http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: &tls.Config{
+					InsecureSkipVerify: c.TLSInsecureSkipVerify,
+				},
+			},
+			Jar: jar,
+		}
+	}
+
+	return nil
+}
+
+// Do performs a given http.Request via the Client's http.Client.
+func (c *Client) Do(r *http.Request) (*http.Response, error) {
+	if err := c.httpClientPrep(); err != nil {
+		return nil, err
+	}
+
+	return c.httpClient.Do(r)
 }
