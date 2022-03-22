02305bf18d2aa5c183244ec007798d91d6d6a0f1
Micah Kemp <micahk@splunk.com>
2022-03-22 10:30:52 -0500

    Added client package description and authentication examples

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package client implements a client to the Splunk REST API.
 package client
 
 import (
